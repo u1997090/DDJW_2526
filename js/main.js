2cf4b0163bde5421579e599acfc0352319591f19
u1997090 <u1997090@campus.udg.edu>
2026-03-15 23:21:25 +0100

    Correcció: arreglat el botó Jugar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+import $ from "../library/jquery-4.0.0.slim.module.min.js";
+
 $(document).ready(function() {
 
     $("#play").on("click", function() {
         let alias = prompt("Introdueix el teu àlies:");
         console.log("Àlies del jugador:", alias);
+
+        window.location.assign("html/game.html");
     });
 
     $("#options").on("click", function() {
